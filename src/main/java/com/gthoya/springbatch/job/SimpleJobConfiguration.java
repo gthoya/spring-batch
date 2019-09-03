58c07255838d46d90c150105edff27c098335566
jacob <jacob.gt@kakaocorp.com>
2019-09-03 17:12:00 +0900

    Initial commit

---
+++ src/main/java/com/gthoya/springbatch/job/SimpleJobConfiguration.java
@@ -0,0 +1,38 @@
+package com.gthoya.springbatch.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    public SimpleJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+    }
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("simpleJob")
+                .start(simpleStep1())
+                .build();
+    }
+
+    @Bean
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("test simple step1");
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
